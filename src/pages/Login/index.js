7d4096d5bcd2ec982d03faa011e944f1b95cec10
kaioa1meida <kaiohsilva04@gmail.com>
2024-10-12 02:37:58 -0300

    feat: create Login page and Styles

---
+++ src/pages/Login/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import api from '../../services/api'
+
+import './styles.css';
+import logoImage from '../../assets/star.svg'
+
+export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function login(e){
+        e.preventDefault();
+
+        const data = {
+            username, 
+            password
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+
+            navigate('/books')
+        } catch (error) {
+            alert('Login Failed! Try Again!');
+        }
+
+    }
+
+    return (
+        <div className="login-container">
+            <section className="form">
+
+                <img src={logoImage} alt="Starcode"/> 
+                <form onSubmit={login}> 
+                    <h1>Access Your Account</h1>
+                    <input 
+                        placeholder="Username"
+                        value={username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className="button" type="submit">Login</button>
+                </form>
+
+            </section>
+        </div>
+    );
+}
