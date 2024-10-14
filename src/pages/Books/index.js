dc1a058561a1cd07a09d64c4d5518c4915c60bf3
kaioa1meida <kaiohsilva04@gmail.com>
2024-10-14 17:45:33 -0300

    feat: adding new methods and modify button color

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,96 +1,98 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom'
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css';
 import logoImage from '../../assets/star.svg'
 
 export default function Books() {
+
+    const [books, setBooks] = useState([]);
+    const [page, setPage] = useState(0);
+
+   
+    const navigate = useNavigate();
+
+    const accessToken = localStorage.getItem('accessToken')
+    const username = localStorage.getItem('username')
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    useEffect(() => {
+        fetchMoreBooks();
+    }, [accessToken])
+
+    async function fetchMoreBooks() {
+        const response = await api.get(`/api/v1/book?page=${page}&limit=4&direction=asc`, authorization);
+        setBooks([ ...books, ...response.data._embedded.bookVOList ]);
+        setPage(page + 1);
+    }
+
+    async function editBook(id) {
+        try {
+            navigate(`/book/new/${id}`);
+        } catch (error) {
+            alert('Edit Failed! Try Again!')
+        }
+    }
+    async function deleteBook(id) {
+        try {
+            await api.delete(`/api/v1/book/${id}`, authorization)
+            setBooks(books.filter(book => book.id !== id))
+        } catch (error) {
+            alert('Delete Failed! Try Again!')
+        }
+    }
+
+    async function logout() {
+        try {
+            localStorage.clear();
+            navigate('/')
+        } catch (error) {
+            alert('Logout Failed! Try Again!')
+        }
+    }
+
     return (
         <div className="book-container">
             <header>
                 <img src={logoImage} alt="starcode"/>
-                <span>Welcome, <strong>Kazzy</strong>!</span>
-                <Link className="button" to="book/new">Add New Book</Link>
-                <button type="button">
-                    <FiPower size={18} color="#251FC5" />
+                <span>Welcome, <strong>{username}</strong>!</span>
+                <Link className="button" to="/book/new/0">Add New Book</Link>
+                <button onClick={logout} type="button">
+                    <FiPower size={18} color="#00BFFF" />
                 </button>
             </header>
             <h1>Registered Books</h1>
             <ul>
-                <li>
+                {books.map(book => (
+                    <li key={book.id}>
                     <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
+                    <p>{book.title}</p>
                     <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
+                    <p>{book.author}</p>
                     <strong>Price:</strong>
-                    <p>R$ 47,90</p>
+                    <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
                     <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
+                    <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
 
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
+                    <button onClick={() => editBook(book.id)} type="button">
+                        <FiEdit size={20} color="#00BFFF" />
                     </button>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
+                    <button onClick={() => deleteBook(book.id)} type="button">
+                        <FiTrash2 size={20} color="#00BFFF" />
                     </button>
                 </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#251FC5" />
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#251FC5" />
-                    </button>
-                </li>
+                ))}
             </ul>
+            <button className='button' onClick={fetchMoreBooks}>Load More...</button>
         </div>
     );
 }
--- src/pages/NewBook/index.js
+++ src/pages/NewBook/index.js
@@ -1,30 +1,115 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css';
 import logoImage from '../../assets/star.svg'
 
-export default function newBook() {
+export default function NewBook() {
+
+    const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+    const [title, setTitle] = useState('');
+
+    const { bookId } = useParams();
+   
+    const navigate = useNavigate();
+
+    const accessToken = localStorage.getItem('accessToken')
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    useEffect(() => {
+        if (bookId === '0') {
+            return;
+        } else {
+            loadBooks();
+        }
+    }, [bookId]);
+
+    async function loadBooks() {
+        try {
+            const response = await api.get(`/api/v1/book/${bookId}`, authorization)
+
+            let adjustedDate = response.data.launchDate.split("T", 10)[0];
+
+            setId(response.data.id)
+            setTitle(response.data.title)
+            setAuthor(response.data.author)
+            setPrice(response.data.price)
+            setLaunchDate(adjustedDate)
+        } catch (error) {
+            alert('Error recovering Book! Try Again');
+            navigate('/books')
+        }
+    }
+
+    async function saveOrUpdate(e){
+        e.preventDefault();
+
+        const data = {
+            author, 
+            launchDate,
+            price,
+            title
+        };
+
+        try {
+            if (bookId === '0') {
+                await api.post('/api/v1/book', data, authorization);
+            } else {
+                data.id = bookId;
+                await api.put('/api/v1/book', data, authorization);
+            }
+        } catch (error) {
+            alert('Book Creation Failed! Try Again!');
+        }
+        navigate('/books')
+    };
+
     return (
         <div className="new-book-container">
             <div className="content">
                 <section className="form">
                     <img src={logoImage} alt="Starcode" />
-                    <h1>Add New Book</h1>
-                    <p>Enter the book information and click on 'Add'!</p>
+                    <h1>{bookId === '0' ? 'Add New' : 'Update'} Book</h1>
+                    <p>Enter the book information and click on {bookId === '0' ? `'Add'` : `'Update'`}!</p>
                     <Link className="back-link" to="/books">
-                        <FiArrowLeft size={16} color="#251FC5" />
+                        <FiArrowLeft size={16} color="##00BFFF" />
                         Home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="title" />
-                    <input placeholder="Author" />
-                    <input type="date" />
-                    <input placeholder="Price" />
+                <form onSubmit={saveOrUpdate}>
+                    <input
+                        placeholder="Title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder="Author"
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                    />
+                    <input
+                        type="date" 
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder="Price"
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
 
-                    <button className="button" type="submit">Add</button>
+                    <button className="button" type="submit">{bookId === '0' ? 'Add' : 'Update'}</button>
                 </form>
             </div>
         </div>
